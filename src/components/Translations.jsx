341069e079b7d6dd6e22cf8d88b68d52efe8e8fc
Your Name <skolnieks@vtdt.edu.lv>
2026-01-22 15:04:06 +0200

    Atjaunoti gherkini

--- src/components/Translations.jsx
+++ src/components/Translations.jsx
@@ -1,17 +1,20 @@
 import TranslationTable from "./TranslationTable";
 
 const translations = [
-  { en: "Feature", lv: "Funkcionalitāte Fīča" },
-  { en: "Background", lv: "Konteksts Situācija" },
-  { en: "Rule", lv: "Rule" },
-  { en: "Scenario", lv: "Piemērs Scenārijs" },
-  { en: "Scenario Outline", lv: "Scenārijs pēc parauga" },
-  { en: "Examples", lv: "Piemēri Paraugs" },
+  { en: "Feature", lv: "Funkcionālā iezīme" },
+  { en: "Scenario", lv: "Scenārijs" },
   { en: "Given", lv: "Kad" },
   { en: "When", lv: "Ja" },
   { en: "Then", lv: "Tad" },
   { en: "And", lv: "Un" },
-  { en: "But", lv: "Bet" }
+  { en: "Or", lv: "Vai" },
+  { en: "But", lv: "Bet" },
+  { en: "Background", lv: "Konteksts" },
+  { en: "Rule", lv: "Noteikums" },
+  { en: "Scenario Outline", lv: "Scenārijs pēc parauga" },
+  { en: "Examples", lv: "Piemēri" },
+  { en: "User Story", lv: "Lietotājstāsts" },
+  { en: "#", lv: "#(komentārs)" },
 ];
 
 function Translations() {
